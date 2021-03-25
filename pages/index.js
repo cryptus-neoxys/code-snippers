5bc9d70185a5ad48b43a3c6ffc22cc766a193c29
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-25 14:33:06 +0530

    title change -minor fix

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-blue-400 m-0 p-0">
+    <div className="flex flex-col items-center justify-center min-h-screen p-0 py-2 m-0 bg-blue-400">
       <Head>
-        <title>Create Next App</title>
+        <title>Code-Snippers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-start flex-1 px-20 text-center bg-white m-0 py">
+      <main className="py flex flex-col items-center justify-start flex-1 px-20 m-0 text-center bg-white">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
