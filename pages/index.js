1910a660b70050265f682ae085120b2289d17feb
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-25 14:09:22 +0530

    initial commit: Next and Tailwind Setup

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center bg-white m-0">
+      <main className="flex flex-col items-center justify-start flex-1 px-20 text-center bg-white m-0 py-4">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
