d62e4c3e13131e927dc9ab6a6e9902a5df93f079
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-28 06:05:40 +0530

    typo fix

--- pages/new.js
+++ pages/new.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import SnippetForm from "../components/SnippetForm";
+import SnippetForm from "../Components/SnippetForm";
 
 export default function Home() {
   return (
