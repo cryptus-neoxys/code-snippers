5390f513a1966970cdb426ba8f31579cde94057c
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-30 02:03:30 +0530

    Added link to git repo + meta desc

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,28 @@ export default function Home() {
       <Head>
         <title>Code-Snippers</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="A website to save, share and create snippets from your everyday coding adventures"
+        />
       </Head>
 
       <main>
         <div className="my-12">
-          <h1 className="text-blue-50 text-2xl">Code-Snippers</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="text-blue-50 text-2xl">Code-Snippers</h1>
+            <a
+              target="_blank"
+              rel="author"
+              href="https://github.com/cryptus-neoxys/code-snippers">
+              <img
+                className="inline-block h-8"
+                src="https://www.vectorlogo.zone/logos/github/github-tile.svg"
+                alt="Checkout GitHub"
+                srcset=""
+              />
+            </a>
+          </div>
           <p className="text-blue-100">
             Create, save and share your code snippets from everyday coding
             adventures.
