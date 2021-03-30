4a793461b93ba03e4efbc6c163be3094f4f9f385
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-30 15:51:49 +0530

    minor ui fix Code -- overflow scroll

--- Components/Code.js
+++ Components/Code.js
@@ -21,11 +21,13 @@ export default function Code({ code }) {
       </button>
       {showCode && (
         <div className="relative">
-          <pre className="p-2 text-gray-800 bg-gray-300 rounded-md">{code}</pre>
+          <pre className="px-2 py-4 pt-8 overflow-scroll text-gray-800 bg-gray-300 rounded-md">
+            {code}
+          </pre>
           <button
             className={`${
               copyText === "Copy" ? "text-gray-600" : "text-green-600"
-            } font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline mb-2 absolute top-0 right-0 transform -translate-x-1 -translate-y-1`}
+            } font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline mb-2 absolute top-2 right-2 transform -translate-x-1 -translate-y-1`}
             type="submit"
             onClick={copyCode}>
             {copyText}
