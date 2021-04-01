5d02edb1438749321d6900c570485b384485b91b
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-02 02:55:16 +0530

    nightwind config

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ThemeProvider } from "next-themes";
 
 import "../styles/app.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
-      <div className="w-full min-h-screen p-10 bg-blue-600">
-        <div className="max-w-2xl mx-auto">
-          <Component {...pageProps} />
+      <ThemeProvider
+        attribute="class"
+        storageKey="nightwind-mode"
+        defaultTheme="dark" // default "light"
+      >
+        <div className="w-full min-h-screen p-10 bg-blue-100">
+          <div className="max-w-2xl mx-auto">
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </UserProvider>
   );
 }
