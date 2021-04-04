9c612ab365050b358f0beead4064acfbdea732b8
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-05 03:42:07 +0530

    Added Code Highlighting (w/themes)

--- Components/Code.js
+++ Components/Code.js
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import vsDark from "prism-react-renderer/themes/vsDark";
+import vsLight from "prism-react-renderer/themes/vsLight";
+import { useTheme } from "next-themes";
 
-export default function Code({ code }) {
+export default function Code({ code, language }) {
   const [showCode, setShowCode] = useState(false);
   const [copyText, setCopyText] = useState("Copy");
 
+  const { theme, setTheme } = useTheme();
+
+  const languages = {
+    Text: "markup",
+    HTML: "markup",
+    C: "c",
+    "C++": "cpp",
+    CSS: "css",
+    Python: "python",
+    JavaScript: "javascript",
+    Typescript: "typescript",
+    JSX: "jsx",
+    TSX: "tsx",
+    Nodejs: "js-extras",
+    JSON: "json",
+    MarkDown: "markdown",
+    Bash: "bash",
+    GraphQL: "graphql",
+    Go: "go",
+    Java: "clike",
+    SASS: "sass",
+    SCSS: "scss",
+    "Objective-C": "objectivec",
+    Caml: "ocaml",
+    SQL: "sql",
+    "Web Assembly": "wasm",
+    Yaml: "yaml",
+  };
   const copyCode = async () => {
     await navigator.clipboard.writeText(code);
     setCopyText("✅ Copied!");
@@ -21,9 +53,25 @@ export default function Code({ code }) {
       </button>
       {showCode && (
         <div className="relative">
-          <pre className="px-2 py-4 pt-8 overflow-scroll text-gray-800 bg-gray-300 rounded-md">
-            {code}
-          </pre>
+          <Highlight
+            {...defaultProps}
+            code={code}
+            language={languages[language]}
+            theme={theme === "dark" ? vsDark : vsLight}>
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+              <pre
+                className={`${className} px-2 py-4 pt-8 overflow-scroll rounded-md`}
+                style={style}>
+                {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                ))}
+              </pre>
+            )}
+          </Highlight>
           <button
             className={`${
               copyText === "Copy" ? "text-gray-700" : "text-green-600"
@@ -37,3 +85,33 @@ export default function Code({ code }) {
     </div>
   );
 }
+
+/* 
+{
+  "HTML": "markup",
+  "Text": "markup      
+  "Bash": "bash",    
+  "Java": "clike",    
+  "C": "c",
+  "C++": "cpp",  
+  "CSS": "css",  
+  "JavaScript": "javascript",          
+  "JSX: "jsx",  
+  "Nodejs": "js-extras",        
+  "Go": "go",
+  "GraphQL": "graphql
+  "JSON": "json",
+  "MarkDown": "markdown",
+  "Objective-C": "objectivec",
+  "Caml": "ocaml",
+  "Python": "python
+  "SASS": "sass",    
+  "SCSS": "scss",
+  "SQL": "sql",
+  : "stylus",
+  "TSX": "tsx",
+  "Typescript": "typescript",
+  "Web Assembly": "wasm",
+  "Yaml": "yaml",
+}
+*/
